e079448de239a5479fb8c8d50477aa0ebb2d3613
msfukui <msfukui@gmail.com>
2017-01-17 21:02:07 +0900

    rbenv の upgrade に雑に対応。

--- rbenv_setup.sh
+++ rbenv_setup.sh
@@ -4,11 +4,19 @@ if [ ! -d "${HOME}/.rbenv/bin" ]; then
   pushd ${HOME}
   git clone https://github.com/sstephenson/rbenv.git .rbenv
   popd
+else
+  pushd ${HOME}/.rbenv
+  git pull origin master
+  popd
 fi
 
 if [ ! -d "${HOME}/.rbenv/plugins/ruby-build" ]; then
   mkdir -p ${HOME}/.rbenv/plugins
   pushd ${HOME}/.rbenv/plugins
   git clone https://github.com/sstephenson/ruby-build.git
+  popd
+else
+  pushd ${HOME}/.rbenv/plugins/ruby-build
+  git pull origin master
   popd
 fi
