9b06fca3bbeada5e3e53aca9505709dd2a28a722
msfukui <msfukui@gmail.com>
2017-04-01 20:33:03 +0900

    Not specifty version to ruby installation.

--- rbenv_ruby_setup.sh
+++ rbenv_ruby_setup.sh
@@ -5,24 +5,35 @@ if [ ! -d "${HOME}/.rbenv/bin" ]; then
   exit 1
 fi
 
-setup_ruby_version=2.3.3
-
 export PATH="$HOME/.rbenv/bin:$PATH"
 eval "$(rbenv init -)"
 
+if [ $# -gt 0 ]; then
+  setup_ruby_version=$1
+else
+  setup_ruby_version=`rbenv install --list | awk '{print $1}' | egrep "^[0-9]+\.[0-9]+\.[0-9]+$" | tail -1`
+fi
+echo "setup_ruby_version: $setup_ruby_version"
+
 rbenv rehash
-rbenv install ${setup_ruby_version}
+# for macports
+if [ -x /opt/local/bin/port ]; then
+  RUBY_CONFIGURE_OPTS="--with-openssl-dir=/opt/local --with-readline-dir=/opt/local" rbenv install ${setup_ruby_version}
+else
+  rbenv install ${setup_ruby_version}
+fi
 rbenv global ${setup_ruby_version}
 rbenv rehash
-ruby -v
 
 gem update --system
 gem update
 rbenv rehash
-gem -v
 
 gem install bundler pry rubocop --no-ri --no-rdoc
 rbenv rehash
+
+ruby -v
+gem -v
 bundle -v
 pry -v
 rubocop -v
