3b32fff4eefd6c5803fdaf986e00b26088701a5f
msfukui <msfukui@gmail.com>
2019-05-12 13:37:11 +0900

    ndenv から nodenv に乗り換え

---
+++ nodenv_nodejs_setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -f
+
+if [ ! -d "${HOME}/.nodenv/bin" ]; then
+  echo "[error] nodenv is not set up."
+  exit 1
+fi
+
+setup_nodejs_version=10.15.3
+
+export PATH="$HOME/.nodenv/bin:$PATH"
+eval "$(nodenv init -)"
+
+nodenv rehash
+nodenv install ${setup_nodejs_version}
+nodenv global ${setup_nodejs_version}
+nodenv rehash
+node -v
+
+npm install -g npm-check-updates
+nodenv rehash
+ncu --version
---
+++ nodenv_setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -f
+
+if [ ! -d "${HOME}/.nodenv/bin" ]; then
+  pushd ${HOME}
+  git clone https://github.com/nodenv/nodenv .nodenv
+  popd
+fi
+
+if [ ! -d "${HOME}/.nodenv/plugins/node-build" ]; then
+  mkdir -p ${HOME}/.nodenv/plugins
+  pushd ${HOME}/.nodenv/plugins
+  git clone https://github.com/nodenv/node-build ${HOME}/.nodenv/plugins/node-build
+  popd
+fi
+
