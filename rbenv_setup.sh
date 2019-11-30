030b676317a545e01f18bb83b076022a614693fb
msfukui <msfukui@gmail.com>
2019-11-30 20:28:01 +0900

    Reflect rbenv repo path changes

--- rbenv_setup.sh
+++ rbenv_setup.sh
@@ -2,7 +2,7 @@
 
 if [ ! -d "${HOME}/.rbenv/bin" ]; then
   pushd ${HOME}
-  git clone https://github.com/sstephenson/rbenv.git .rbenv
+  git clone https://github.com/rbenv/rbenv.git .rbenv
   popd
 else
   pushd ${HOME}/.rbenv
@@ -13,7 +13,7 @@ fi
 if [ ! -d "${HOME}/.rbenv/plugins/ruby-build" ]; then
   mkdir -p ${HOME}/.rbenv/plugins
   pushd ${HOME}/.rbenv/plugins
-  git clone https://github.com/sstephenson/ruby-build.git
+  git clone https://github.com/rbenv/ruby-build.git
   popd
 else
   pushd ${HOME}/.rbenv/plugins/ruby-build
